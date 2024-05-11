c2aca9ba31f950173ea85d9878dfdb009376b8d5
Raquel <raquelmoreira847@gmail.com>
2024-05-11 11:28:59 -0300

    criando um swiper slider para os eventos em destaque

--- vivaperifa/src/static/Header/CarrosselEventoDestaque/CarrosselEventoDestaque.jsx
+++ vivaperifa/src/static/Header/CarrosselEventoDestaque/CarrosselEventoDestaque.jsx
@@ -1,43 +1,66 @@
 import EventoDestaque from '../EventoDestaque/EventoDestaque';
 import './CarrosselEventoDestaque.css';
 import Xamuel from '../../../assets/xamuel.jpg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 export default function CarrosselEventoDestaque(){
     const listItem = [
         {
+            id: 1,
             imagem: Xamuel,
             titulo: "Batalha de Rima em SP",
             data: "Sábado, dia 20/04 às 9h - 12h, SP Jardim Clímax"
         },
         {
+            id: 2,
+            imagem: Xamuel,
+            titulo: "Batalha de Rima em SP",
+            data: "Sábado, dia 20/04 às 9h - 12h, SP Jardim Clímax"
+        },
+        {
+            id: 3,
             imagem: Xamuel,
             titulo: "Batalha de Rima em SP",
             data: "Sábado, dia 20/04 às 9h - 12h, SP Jardim Clímax"
         }
     ]
-
-    var itens = document.querySelectorAll("itens");
-    var currentItem = 0;
-
-    console.log(itens);
-    function scroll(){
-        currentItem++;
-        itens[currentItem].scrollIntoView({
-            inline: "center",
-            behavior: "smooth",
-            block: "center"
-        });
-    }
     return(
         <div className='evento-destaque-scroll'>
-            <div className='container-fixed itens'>
+            {/* <div className='container-fixed itens'>
                 {
                     listItem.map((item, index) => {
-                        return <EventoDestaque tituloEventoDestaque={item.titulo} imagemEventoDestaque={item.imagem} dataEventoDestaque={item.data}></EventoDestaque>
+                        return <EventoDestaque 
+                                key={index}
+                                tituloEventoDestaque={item.titulo} 
+                                imagemEventoDestaque={item.imagem} 
+                                dataEventoDestaque={item.data}
+                            />
                     })
                 }
-            </div>
-            <button id='arrow-right' onClick={scroll}>Direita</button>
+            </div> */}
+
+            <Swiper
+                slidesPerView={1}
+                pagination={{clickable:true}}
+                navigation
+            >
+                {
+                    listItem.map((item) => (
+                        <SwiperSlide ke={item.id}>
+                            <div className='item'>
+                                <EventoDestaque
+                                    tituloEventoDestaque={item.titulo} 
+                                    imagemEventoDestaque={item.imagem} 
+                                    dataEventoDestaque={item.data}
+                                />
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
+
+            </Swiper>
+            <button id='arrow-right'>Direita</button>
             <button id='arrow-left'>Esquerda</button>
         </div>
     );
