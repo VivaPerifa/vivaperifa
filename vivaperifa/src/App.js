51bac94e1770cd2fba86879933a42aa015e103d3
Erick Gabriel <159559836+ErickGabrielDev@users.noreply.github.com>
2024-05-10 22:33:48 -0300

    primeiro commit

--- vivaperifa/src/App.js
+++ vivaperifa/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import CarrosselEventoDestaque from './static/Header/CarrosselEventoDestaque/CarrosselEventoDestaque';
+import SobreNos from './pages/SobreNos/SobreNos';
+
 
 function App() {
   return (
     <div className='container'>
       <CarrosselEventoDestaque></CarrosselEventoDestaque>
+      <SobreNos></SobreNos>
     </div>
+    
   );
 }
 
