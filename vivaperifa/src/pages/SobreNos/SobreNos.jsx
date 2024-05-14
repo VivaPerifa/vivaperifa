bc7f06b391eb558882dc51131e9c4ed631b0d99e
Erick Gabriel <159559836+ErickGabrielDev@users.noreply.github.com>
2024-05-13 21:58:55 -0300

    finalizando a equipe

--- vivaperifa/src/pages/SobreNos/SobreNos.jsx
+++ vivaperifa/src/pages/SobreNos/SobreNos.jsx
@@ -4,6 +4,20 @@ import visao from "../../assets/iconeVissao.png";
 import valores from "../../assets/iconeValores.png";
 import logo from "../../assets/logoVivaPerifa.png";
 import imagemPerifa from "../../assets/ImagemPerifa.png";
+import jessica from "../../assets/imagemEquipeJessica.png";
+import raquel from "../../assets/imagemEquipeRaquel.png";
+import larissa from "../../assets/imagemEquipeLarissa.png";
+import erick from "../../assets/imagemEquipeErick.png";
+import nathan from "../../assets/imagemEquipeNathan.png";
+import renan from "../../assets/imagemEquipeRenan.png";
+import diego from "../../assets/imagemEquipeDiego.png";
+import kaue from "../../assets/imagemEquipeKaue.png";
+
+
+
+
+
+
 
 
 export default function SobreNos() {
@@ -63,6 +77,71 @@ export default function SobreNos() {
                     <img src={imagemPerifa} alt="" />
                 </div>
             </div>
+
+
+
+
+            <div className="conteudo-equipe">
+                <div className="equipe">
+                    <h1 className="title-equipe">Equipe</h1>
+                </div>
+                <div className="equipe-1">
+
+                    <div className="jessica">
+                        <img src={jessica} alt="" />
+                        <h1>Jéssica Souza</h1>
+                        <p>product Owner & Dev FullStack</p>
+                    </div>
+
+                    <div className="raquel">
+                        <img src={raquel} alt="" />
+                        <h1>raquel Costa</h1>
+                        <p>Scrum Master & Dev FullStack</p>
+                    </div>
+
+                    <div className="larissa">
+                        <img src={larissa} alt="" />
+                        <h1>Larissa Dias</h1>
+                        <p>Financeiro & Dev Front-End</p>
+                    </div>
+
+                    <div className="erick">
+                        <img src={erick} alt="" />
+                        <h1>Erick Gabriel</h1>
+                        <p>Dev Front-End</p>
+                    </div>
+                </div>
+
+
+                <div className="equipe-2">
+                    <div className="nathan">
+                        <img src={nathan} alt="" />
+                        <h1>nathan Rabelo</h1>
+                        <p>Dev FullStack</p>
+                    </div>
+
+                    <div className="renan">
+                        <img src={renan} alt="" />
+                        <h1>Renan Anacleto</h1>
+                        <p>Dev FullStack & Designer</p>
+                    </div>
+
+                    <div className="diego">
+                        <img src={diego} alt="" />
+                        <h1>Diego Guarnieri</h1>
+                        <p>Dev Front-end & Designer</p>
+                    </div>
+
+                    <div className="kaue">
+                        <img src={kaue} alt="" />
+                        <h1>Kaue Souza</h1>
+                        <p>Dev Back-End</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
+
     )
 }
