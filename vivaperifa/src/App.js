7a98b6f935ed8a8f728792580013e130c4005f51
ERICK GABRIEL FERREIRA AGOSTINHO <erick.gfagostinho@senacsp.edu.br>
2024-05-16 12:08:41 -0300

    atualizando o sobre nós

--- vivaperifa/src/App.js
+++ vivaperifa/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className='container'>
       <CarrosselEventoDestaque></CarrosselEventoDestaque>
-      {/* <SobreNos></SobreNos> */}
+      <SobreNos></SobreNos> 
     </div>
     
   );
