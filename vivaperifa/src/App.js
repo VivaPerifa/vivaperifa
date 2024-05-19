6bedb47882e0c94c33bc3214a02602e5bba06019
DiegoEmmanuel <161338189+DiegoEmmanuelDev@users.noreply.github.com>
2024-05-19 11:24:08 -0300

    Criando Evento comum

--- vivaperifa/src/App.js
+++ vivaperifa/src/App.js
@@ -3,12 +3,14 @@ import CarrosselEventoDestaque from './static/Header/CarrosselEventoDestaque/Car
 import SobreNos from './pages/SobreNos/SobreNos';
 import EventoDestaque from './static/Header/EventoDestaque/EventoDestaque';
 import Menu from './static/Header/Menu/Menu';
+import EventoComum from './components/CarrosselEvento/EventoComum/EventoComum';
 
 function App() {
   return (
     <div className='container'>
       <CarrosselEventoDestaque></CarrosselEventoDestaque>
       <SobreNos></SobreNos> 
+      <EventoComum>giu</EventoComum>
     </div>
     
   );
