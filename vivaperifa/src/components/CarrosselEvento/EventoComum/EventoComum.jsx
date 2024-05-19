637a4d7b92907ce78efac556ebca8d4ee1997812
Raquel <raquelmoreira847@gmail.com>
2024-05-19 13:21:20 -0300

    arrumando as dimensões do evento comum

--- vivaperifa/src/components/CarrosselEvento/EventoComum/EventoComum.jsx
+++ vivaperifa/src/components/CarrosselEvento/EventoComum/EventoComum.jsx
@@ -3,65 +3,51 @@ import FotoEvento from '../../../assets/imagem-evento.jpg';
 import Coracao from '../../../assets/coracao.png';
 import Agenda from '../../../assets/agenda.png';
 
-export default function EventoComum(){
-    return(
-
+export default function EventoComum() {
+    return (
         <div className="container-evento">
+            <div className="evento-comum">
 
-          <div className="evento-comum">
- 
-               <figure className="imagem-evento-comum">
-                          
-                          <img src={FotoEvento} alt="" />
+                <figure className="imagem-evento-comum">
+                    <img src={FotoEvento} alt="" />
+                </figure>
 
-               </figure>
+                <div className="dados-evento-comum">
+                    <div className='titulo-evento-comum'>
+                        <h2>
+                            Tenda de historia
+                        </h2>
+                    </div>
 
-               <div className="titulo-evento-comum">
+                    <div className='icones-evento-comum'>
+                        <span>
+                            <img src={Coracao} alt="" />
+                        </span>
 
-             <div>
-                    <h2>
-                        Tenda de historia
-                    </h2>
-             </div>
-               
-                <div className='icones'>
+                        <span>
+                            <img src={Agenda} alt="" />
+                        </span>
+                    </div>
+                </div>
+
+                <div className="data-evento-comum">
                     <span>
-                        <img src={Coracao} alt="" />
-                    </span>
-
-                    <span>
-                        <img src={Agenda} alt="" />
+                        data
                     </span>
                 </div>
 
-               </div>
+                <div className='descricao-evento-comum'>
+                    <p>
+                        descricao
+                    </p>
+                </div>
 
-               <div className="data-evento-comum">
-
-                     <span>
-                        data
-                     </span>
-
-               </div>
-
-               <div className='descricao-evento-comum'>
-
-                  <p>
-                    descricao
-                  </p>
-
-               </div>
-
-               <div className='local-evento-comum'>
-
-               <span>
-                    local
-               </span>
-
-               </div>
- 
-          </div>
-
+                <div className='local-evento-comum'>
+                    <span>
+                        local
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
