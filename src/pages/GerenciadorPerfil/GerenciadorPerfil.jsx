6769b0bb85855deb3c457ed184f7c8e165923295
Nathan Viana <nathanviana2007@hotmail.com>
2024-06-14 13:55:15 -0300

    merge

--- src/pages/GerenciadorPerfil/GerenciadorPerfil.jsx
+++ src/pages/GerenciadorPerfil/GerenciadorPerfil.jsx
@@ -14,7 +14,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 export default function GerenciadorPerfil() {
     return (     
         <div className="gerenciar-perfil">
-            <MenuLateralPerfil></MenuLateralPerfil>
             <CadastrarEvento></CadastrarEvento>
         </div>
     );
