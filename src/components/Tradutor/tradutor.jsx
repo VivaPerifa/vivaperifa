7dde92544fb64cc00bc5ac4ea53dad3438ec28d3
Nathan Viana <nathanviana2007@hotmail.com>
2024-06-17 00:58:14 -0300

    Atualizando login

--- src/components/Tradutor/tradutor.jsx
+++ src/components/Tradutor/tradutor.jsx
@@ -1,10 +1,20 @@
+import './tradutor.css';
 import React from 'react';
+import EUA from '../../assets/bandeira_eua.png';
+import Brasil from '../../assets/bandeira_brasil.png';
 
 const Tradutor = ({ changeLanguage }) => {
   return (
-    <div>
-      <button onClick={() => changeLanguage('en')}>English</button>
-      <button onClick={() => changeLanguage('pt')}>Português</button>
+    <div className='btn_translate'>
+
+      <div className="btn_brasil">
+        <img src={Brasil} alt="" className='br' onClick={() => changeLanguage('pt')} />
+      </div>
+
+      <div className="btn_eua">
+        <img src={EUA} alt="" className='us' onClick={() => changeLanguage('en')} />
+      </div>
+
     </div>
   );
 };
