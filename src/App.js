3bc175de85d3128322d420838aecfc2656c8c956
Erick Gabriel <159559836+ErickGabrielDev@users.noreply.github.com>
2024-06-18 00:05:22 -0300

    merge

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ function App() {
       <BotaoTopo/>
       {/* <EventosCadastrados></EventosCadastrados> */}
       <VisualizarEvento></VisualizarEvento>
+      <PerfilParticipante></PerfilParticipante>
     </BrowserRouter>
   );
 }
